564dc0f1f92b7160be343271610c13d5812218fc
vietdeptraivtc <88416296+vietdeptraivtc@users.noreply.github.com>
2024-06-12 01:45:01 +0700

    Add files via upload

---
+++ src/TreeNode.java
@@ -0,0 +1,15 @@
+public class TreeNode {
+    private int value;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+
+    public void setValue(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
